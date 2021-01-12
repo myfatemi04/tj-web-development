55a6378160b97db99c0aaeee67c80bc8fa3bc3a0
Michael Fatemi <myfatemi04@gmail.com>
2021-01-12 15:27:34 -0500

    Added log in/log out messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const cookieSession = require("cookie-session");
+const { getProfile } = require("./auth");
 const app = express();
 
 app.set("view engine", "hbs");
@@ -30,9 +31,17 @@ app.use("/weather", require("./Weather"));
 app.use("/geography_quiz", require("./GeographyQuiz"));
 app.use("/auth", require("./auth").authRouter);
 
-app.get("/", (req, res) => {
-	res.locals.title = "Welcome to my website!";
-	res.render("index");
+app.get("/", async (req, res) => {
+	let title = "Welcome to my website!";
+	if (req.session.accessToken) {
+		let profile = await getProfile(req.session.accessToken);
+		title = "Welcome back, " + profile.short_name + "!";
+	}
+
+	res.render("index", {
+		isLoggedIn: !!req.session.accessToken,
+		title,
+	});
 });
 
 const port = process.env.PORT || 3000;
